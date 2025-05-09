4555d130c8e7773650ed5a0e1a2d010770663f54
0-Meng-bits <mugiwaraomeng@gmail.com>
2025-05-09 21:25:20 +0800

    Create sell.aspx.cs

---
+++ sell.aspx.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.VESTASIM
+{
+    public partial class Sell : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["ClientID"] != null)
+                {
+                    loginPanel.Visible = false;
+                    logoutPanel.Visible = true;
+                }
+                else
+                {
+                    loginPanel.Visible = true;
+                    logoutPanel.Visible = false;
+                }
+            }
+        }
+
+            protected void btnSearch_Click(object sender, EventArgs e)
+            {
+                string searchQuery = txtSearch.Text.Trim();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["YourConnectionString"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                string query = "SELECT * FROM Properties WHERE Title LIKE @SearchQuery OR Description LIKE @SearchQuery";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@SearchQuery", "%" + searchQuery + "%");
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    // Bind the data to the GridView
+                    GridView1.DataSource = reader;
+                    GridView1.DataBind();
+                }
+            }
+    
+    }
+}
